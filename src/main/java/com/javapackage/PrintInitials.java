72ab91d82cbefa413521985f6bb8345fdc0ca7d7
Babakailash <nathkailash2020@gmail.com>
2021-07-16 23:02:24 +0530

    check: Leap Year using Scanner Class

---
+++ src/main/java/com/javapackage/PrintInitials.java
@@ -0,0 +1,15 @@
+package com.javapackage;
+public class PrintInitials {
+    public static void main(String[] args) {
+        int i,j,k;
+        for(i=0; i<9; i++){
+            for (j=i+1; j<9; j++){
+
+                System.out.print("Not Done");
+            }
+            System.out.println("**");
+
+        }
+    }
+
+}
