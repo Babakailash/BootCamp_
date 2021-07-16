a7ba0b8094481d73604fed5bc0cf68769bed3e65
Babakailash <nathkailash2020@gmail.com>
2021-07-16 23:20:27 +0530

    Added:Leap Year Check Always 1582 above

--- src/main/java/com/javapackage/LeapYear.java
+++ src/main/java/com/javapackage/LeapYear.java
@@ -8,11 +8,16 @@ public class LeapYear {
         Scanner sc = new Scanner(System.in);
         year = sc.nextInt();
 
-        if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
-            System.out.println("Specified year is a leap year");
-        } else {
-            System.out.print("Specified year is not a leap year");
+        if (year >= 1582) {
 
+            if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+                System.out.println("Specified year is a leap year");
+            } else {
+                System.out.print("Specified year is not a leap year");
+            }
+        } else {
+            System.out.println("Invalid!!");
         }
     }
 }
+
